6e1c647dfa81931c01b1c1c68d6717e566177c70
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2025-08-05 11:09:20 +0300

    fix(github tests): run tests instead of start:prod

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -6,4 +6,5 @@ echo "🟢 STARTUP SCRIPT VERSION: v2-no-maildev ✅"
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
 npm run seed:run:relational
-npm run start:prod
+# npm run start:prod
+npm run test:e2e

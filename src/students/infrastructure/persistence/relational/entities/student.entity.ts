3a170b16747bc69e9b62061e54f3d544818d9c95
moses <kipronotena@gmail.com>
2026-06-09 13:23:16 +0300

    rfid added

--- src/students/infrastructure/persistence/relational/entities/student.entity.ts
+++ src/students/infrastructure/persistence/relational/entities/student.entity.ts
@@ -55,7 +55,7 @@ export class StudentEntity extends EntityRelationalHelper {
   daily_fee!: number | null;
 
   @Column({ type: 'varchar', nullable: true })
-  rfid_code: string | null;
+  rfid_code!: string | null;
 
   @Column({ type: 'float', nullable: true })
   transport_term_fee!: number | null;

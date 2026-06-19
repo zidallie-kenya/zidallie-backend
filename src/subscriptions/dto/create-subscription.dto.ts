9425b8190d8c7add567bfc0ea18d5235faeb7f5e
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2026-06-19 14:30:37 +0300

    phone validator bug

--- src/subscriptions/dto/create-subscription.dto.ts
+++ src/subscriptions/dto/create-subscription.dto.ts
@@ -11,9 +11,9 @@ export class CreateSubscriptionDto {
   @IsString()
   @IsNotEmpty()
   // This Regex allows +254 followed by 7 or 1, and then 8 digits
-  @Matches(/^\+254[17]\d{8}$/, {
+  @Matches(/^(\+254|254)[17]\d{8}$/, {
     message:
-      'phone_number must be a valid Kenyan phone number (e.g. +2547... or +2541...)',
+      'phone_number must be a valid Kenyan phone number (e.g. +2547... or 2547...)',
   })
   phone_number!: string;
 
